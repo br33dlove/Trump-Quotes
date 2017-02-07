280c0ec4bf162eb63752cd65e489388afd739166
davidcryer <davecryer.dc@gmail.com>
2017-02-07 00:09:49 +0000

    add cancel request method to quote requester

--- app/src/main/java/com/davidcryer/trumpquotes/platformindependent/model/quotes/QuoteRequester.java
+++ app/src/main/java/com/davidcryer/trumpquotes/platformindependent/model/quotes/QuoteRequester.java
@@ -2,4 +2,5 @@ package com.davidcryer.trumpquotes.platformindependent.model.quotes;
 
 public interface QuoteRequester {
     void requestQuote(final QuoteResponseHandler responseHandler);
+    void cancelRequest(final QuoteResponseHandler responseHandler);
 }
