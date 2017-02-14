2618d63139229c62844a67dff7d8df05b12cc7be
David Cryer <davidcryer@Davids-MBP.home>
2017-02-14 16:19:03 +0000

    split quote requester into two - random and personalised

--- app/src/main/java/com/davidcryer/trumpquotes/platformindependent/model/quotes/network/PersonalisedQuoteRequesterImpl.java
+++ app/src/main/java/com/davidcryer/trumpquotes/platformindependent/model/quotes/network/PersonalisedQuoteRequesterImpl.java
@@ -1,82 +1,27 @@
 package com.davidcryer.trumpquotes.platformindependent.model.quotes.network;
 
-import com.davidcryer.trumpquotes.platformindependent.model.quotes.Quote;
-
 import java.util.Set;
 
-public class PersonalisedQuoteRequesterImpl implements PersonalisedQuoteRequester {
+public class PersonalisedQuoteRequesterImpl extends QuoteRequester implements PersonalisedQuoteRequester {
     private final QuoteRequestFactory requestFactory;
-    private final Set<QuoteRequestCallback> callbacks;
-    private Quote lastReceivedQuote;
-    private QuoteRequest request;
 
     public PersonalisedQuoteRequesterImpl(QuoteRequestFactory requestFactory, Set<QuoteRequestCallback> callbacks) {
+        super(callbacks);
         this.requestFactory = requestFactory;
-        this.callbacks = callbacks;
     }
 
     @Override
-    public void requestPersonalisedQuote(String name, QuoteRequestCallback requestCallback, boolean preferLastReceivedQuote) {
-        if (preferLastReceivedQuote && lastReceivedQuote != null) {
-            requestCallback.success(lastReceivedQuote);
-        } else if (request == null) {
-            executeNewRequest(name, requestCallback);
-        }
-    }
-
-    private void executeNewRequest(final String name, final QuoteRequestCallback requestCallback) {
-        callbacks.add(requestCallback);
-        request = requestFactory.personalisedQuoteRequest(name, new QuoteRequestCallback() {
+    public void requestPersonalisedQuote(final String name, QuoteRequestCallback requestCallback, boolean preferLastReceivedQuote) {
+        requestQuote(requestCallback, preferLastReceivedQuote, new RequestProvider() {
             @Override
-            public void success(Quote quote) {
-                clearRequest();
-                lastReceivedQuote = quote;
-                notifyCallbacksOfSuccess(quote);
-            }
-
-            @Override
-            public void failure() {
-                clearRequest();
-                notifyCallbacksOfFailure();
+            public QuoteRequest request() {
+                return requestFactory.personalisedQuoteRequest(name, requestCallback());
             }
         });
-        request.executeAsync();
     }
 
     @Override
     public void remove(QuoteRequestCallback callback, boolean cancelRequest) {
-        callbacks.remove(callback);
-        if (cancelRequest) {
-            cancelRequest();
-        }
-    }
-
-    private void notifyCallbacksOfSuccess(final Quote quote) {
-        for (final QuoteRequestCallback callback : callbacks) {
-            if (callback != null) {
-                callback.success(quote);
-            }
-        }
-        callbacks.clear();
-    }
-
-    private void notifyCallbacksOfFailure() {
-        for (final QuoteRequestCallback callback : callbacks) {
-            if (callback != null) {
-                callback.failure();
-            }
-        }
-        callbacks.clear();
-    }
-
-    private void cancelRequest() {
-        if (request != null) {
-            request.cancel();
-            clearRequest();
-        }
-    }
-
-    private void clearRequest() {
-        request = null;
+        super.remove(callback, cancelRequest);
     }
 }
--- app/src/main/java/com/davidcryer/trumpquotes/platformindependent/model/quotes/network/RandomQuoteRequesterImpl.java
+++ app/src/main/java/com/davidcryer/trumpquotes/platformindependent/model/quotes/network/RandomQuoteRequesterImpl.java
@@ -1,82 +1,27 @@
 package com.davidcryer.trumpquotes.platformindependent.model.quotes.network;
 
-import com.davidcryer.trumpquotes.platformindependent.model.quotes.Quote;
-
 import java.util.Set;
 
-public class RandomQuoteRequesterImpl implements RandomQuoteRequester {
+public class RandomQuoteRequesterImpl extends QuoteRequester implements RandomQuoteRequester {
     private final QuoteRequestFactory requestFactory;
-    private final Set<QuoteRequestCallback> callbacks;
-    private Quote lastReceivedQuote;
-    private QuoteRequest request;
 
     public RandomQuoteRequesterImpl(QuoteRequestFactory requestFactory, Set<QuoteRequestCallback> callbacks) {
+        super(callbacks);
         this.requestFactory = requestFactory;
-        this.callbacks = callbacks;
     }
 
     @Override
     public void requestRandomQuote(QuoteRequestCallback requestCallback, boolean preferLastReceivedQuote) {
-        if (preferLastReceivedQuote && lastReceivedQuote != null) {
-            requestCallback.success(lastReceivedQuote);
-        } else if (request == null) {
-            executeNewRequest(requestCallback);
-        }
-    }
-
-    private void executeNewRequest(final QuoteRequestCallback requestCallback) {
-        callbacks.add(requestCallback);
-        request = requestFactory.randomQuoteRequest(new QuoteRequestCallback() {
+        requestQuote(requestCallback, preferLastReceivedQuote, new RequestProvider() {
             @Override
-            public void success(Quote quote) {
-                clearRequest();
-                lastReceivedQuote = quote;
-                notifyCallbacksOfSuccess(quote);
-            }
-
-            @Override
-            public void failure() {
-                clearRequest();
-                notifyCallbacksOfFailure();
+            public QuoteRequest request() {
+                return requestFactory.randomQuoteRequest(requestCallback());
             }
         });
-        request.executeAsync();
     }
 
     @Override
     public void remove(QuoteRequestCallback callback, boolean cancelRequest) {
-        callbacks.remove(callback);
-        if (cancelRequest) {
-            cancelRequest();
-        }
-    }
-
-    private void notifyCallbacksOfSuccess(final Quote quote) {
-        for (final QuoteRequestCallback callback : callbacks) {
-            if (callback != null) {
-                callback.success(quote);
-            }
-        }
-        callbacks.clear();
-    }
-
-    private void notifyCallbacksOfFailure() {
-        for (final QuoteRequestCallback callback : callbacks) {
-            if (callback != null) {
-                callback.failure();
-            }
-        }
-        callbacks.clear();
-    }
-
-    private void cancelRequest() {
-        if (request != null) {
-            request.cancel();
-            clearRequest();
-        }
-    }
-
-    private void clearRequest() {
-        request = null;
+        super.remove(callback, cancelRequest);
     }
 }
