23a420b5ef45eaa673d413aeb994fd8649639eb5
David Cryer <davidcryer@Davids-MBP.home>
2017-02-15 23:29:39 +0000

    implement dependency injection

--- app/src/main/java/com/davidcryer/trumpquotes/android/framework/application/ApplicationDependencyProvider.java
+++ app/src/main/java/com/davidcryer/trumpquotes/android/framework/application/ApplicationDependencyProvider.java
@@ -1,11 +1,27 @@
 package com.davidcryer.trumpquotes.android.framework.application;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.davidcryer.trumpquotes.android.framework.tasks.factories.ThreadPoolExecutorTaskSchedulerFactory;
+import com.davidcryer.trumpquotes.android.framework.tasks.factories.ThreadPoolExecutorTaskSchedulerFactoryImpl;
 import com.davidcryer.trumpquotes.android.framework.viewwrapperrepositories.ViewWrapperRepositoryFactory;
 import com.davidcryer.trumpquotes.android.framework.viewwrapperrepositories.ViewWrapperRepositoryFactoryImpl;
+import com.davidcryer.trumpquotes.android.model.quotes.network.retrofit.RetrofitQuoteService;
+import com.davidcryer.trumpquotes.android.model.quotes.network.retrofit.RetrofitQuoteRequestFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.network.retrofit.factories.QuoteRetrofitFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.network.retrofit.factories.QuoteRetrofitFactoryImpl;
+import com.davidcryer.trumpquotes.android.model.quotes.network.retrofit.factories.RetrofitQuoteServiceFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.network.retrofit.factories.RetrofitQuoteServiceFactoryImpl;
+import com.davidcryer.trumpquotes.android.model.quotes.store.factories.AndroidQuoteStoreHandlerFactory;
 import com.davidcryer.trumpquotes.android.model.quotes.store.factories.SQLiteQuoteStoreFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.store.tasks.factories.QuoteStoreTaskHandlerFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.store.tasks.factories.QuoteStoreTaskHandlerFactoryImpl;
+import com.davidcryer.trumpquotes.android.model.quotes.store.tasks.factories.QuoteStoreTasksFactoryFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.store.tasks.factories.QuoteStoreTasksFactoryFactoryImpl;
+import com.davidcryer.trumpquotes.android.model.quotes.store.tasks.factories.QuoteStoreThreadPoolExecutorFactory;
+import com.davidcryer.trumpquotes.android.model.quotes.store.tasks.factories.QuoteStoreThreadPoolExecutorFactoryImpl;
 import com.davidcryer.trumpquotes.android.presenter.presenters.factories.PresenterFactoryFactory;
 import com.davidcryer.trumpquotes.android.presenter.presenters.factories.PresenterFactoryFactoryImpl;
 import com.davidcryer.trumpquotes.android.view.viewmodels.factories.QuotesAndroidViewModelFactory;
@@ -14,7 +30,9 @@ import com.davidcryer.trumpquotes.android.view.viewmodels.models.AndroidViewQuot
 import com.davidcryer.trumpquotes.android.view.viewmodels.models.factories.AndroidViewQuoteFactory;
 import com.davidcryer.trumpquotes.android.view.viewwrapperfactories.ViewWrapperFactory;
 import com.davidcryer.trumpquotes.android.view.viewwrapperfactories.ViewWrapperFactoryImpl;
-import com.davidcryer.trumpquotes.platformindependent.model.quotes.network.factories.QuoteRequesterFactory;
+import com.davidcryer.trumpquotes.platformindependent.model.quotes.network.QuoteRequestFactory;
+import com.davidcryer.trumpquotes.platformindependent.model.quotes.network.requesters.factories.QuoteRequesterFactory;
+import com.davidcryer.trumpquotes.platformindependent.model.quotes.network.requesters.factories.QuoteRequesterFactoryImpl;
 import com.davidcryer.trumpquotes.platformindependent.model.quotes.store.factories.QuoteStoreFactory;
 import com.davidcryer.trumpquotes.platformindependent.model.quotes.store.factories.QuoteStoreHandlerFactory;
 import com.davidcryer.trumpquotes.platformindependent.view.viewmodels.models.factories.ViewQuoteFactory;
@@ -30,15 +48,35 @@ class ApplicationDependencyProvider {
     }
 
     private static PresenterFactoryFactory createPresenterFactory(final Context context) {
-        return new PresenterFactoryFactoryImpl(createQuoteRequesterFactory(), createQuoteStoreHandlerFactory(context), createViewQuoteFactory());
+        return new PresenterFactoryFactoryImpl(createQuoteRequesterFactory(context.getResources()), createQuoteStoreHandlerFactory(context), createViewQuoteFactory());
     }
 
-    private static QuoteRequesterFactory createQuoteRequesterFactory() {
-        return null;//TODO
+    private static QuoteRequesterFactory createQuoteRequesterFactory(final Resources resources) {
+        return new QuoteRequesterFactoryImpl(createQuoteRequestFactory(resources));
+    }
+
+    private static QuoteRequestFactory createQuoteRequestFactory(final Resources resources) {
+        return new RetrofitQuoteRequestFactory(createRetrofitQuoteService(resources));
+    }
+
+    private static RetrofitQuoteService createRetrofitQuoteService(final Resources resources) {
+        return createRetrofitQuoteServiceFactory(resources).create();
+    }
+
+    private static RetrofitQuoteServiceFactory createRetrofitQuoteServiceFactory(final Resources resources) {
+        return new RetrofitQuoteServiceFactoryImpl(createQuoteRetrofitFactory(resources));
+    }
+
+    private static QuoteRetrofitFactory createQuoteRetrofitFactory(final Resources resources) {
+        return new QuoteRetrofitFactoryImpl(resources);
     }
 
     private static QuoteStoreHandlerFactory createQuoteStoreHandlerFactory(final Context context) {
-        return null;//TODO
+        return new AndroidQuoteStoreHandlerFactory(createQuoteStoreTasksFactoryFactory(context), createQuoteStoreTaskHandlerFactory());
+    }
+
+    private static QuoteStoreTasksFactoryFactory createQuoteStoreTasksFactoryFactory(final Context context) {
+        return new QuoteStoreTasksFactoryFactoryImpl(createQuoteStoreFactory(context).create());
     }
 
     private static QuoteStoreFactory createQuoteStoreFactory(final Context context) {
@@ -47,6 +85,18 @@ class ApplicationDependencyProvider {
 
     private static SQLiteDatabase.CursorFactory createCursorFactory() {
         return null;
+    }
+
+    private static QuoteStoreTaskHandlerFactory createQuoteStoreTaskHandlerFactory() {
+        return new QuoteStoreTaskHandlerFactoryImpl(createThreadPoolExecutorTaskSchedulerFactory(), createQuoteStoreThreadPoolExecutorFactory());
+    }
+
+    private static ThreadPoolExecutorTaskSchedulerFactory createThreadPoolExecutorTaskSchedulerFactory() {
+        return new ThreadPoolExecutorTaskSchedulerFactoryImpl();
+    }
+
+    private static QuoteStoreThreadPoolExecutorFactory createQuoteStoreThreadPoolExecutorFactory() {
+        return new QuoteStoreThreadPoolExecutorFactoryImpl();
     }
 
     private static ViewQuoteFactory<AndroidViewQuote> createViewQuoteFactory() {
