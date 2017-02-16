c398cd22131f9f6543de3572040b124e22803173
David Cryer <davidcryer@Davids-MBP.home>
2017-02-16 23:44:53 +0000

    Add class to handle alpha animations

---
+++ app/src/main/java/com/davidcryer/trumpquotes/android/view/ui/helpers/AlphaAnimationHelper.java
@@ -0,0 +1,36 @@
+package com.davidcryer.trumpquotes.android.view.ui.helpers;
+
+import android.view.View;
+
+public class AlphaAnimationHelper {
+
+    public static void fadeOut(final View view, final long maxDuration) {
+        view.animate()
+                .alpha(0)
+                .setDuration(alphaAnimationDuration(view.getAlpha(), 0, maxDuration))
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.setVisibility(View.GONE);
+                    }
+                })
+                .start();
+    }
+
+    public static void fadeIn(final View view, final long maxDuration) {
+        view.animate()
+                .alpha(1)
+                .setDuration(alphaAnimationDuration(view.getAlpha(), 1, maxDuration))
+                .withStartAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.setVisibility(View.VISIBLE);
+                    }
+                })
+                .start();
+    }
+
+    private static long alphaAnimationDuration(final float start, final float end, final long maxDuration) {
+        return (long) Math.abs(start - end) * maxDuration;
+    }
+}
