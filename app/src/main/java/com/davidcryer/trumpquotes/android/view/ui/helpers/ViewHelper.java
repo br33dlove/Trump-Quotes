03211aa5d29c8c2608fe2c00b09d0b0ed79d1185
David Cryer <davidcryer@Davids-MBP.home>
2017-02-19 13:11:29 +0000

    Add ViewHelper to return true if coordinates are in view bounds - left as todo

---
+++ app/src/main/java/com/davidcryer/trumpquotes/android/view/ui/helpers/ViewHelper.java
@@ -0,0 +1,14 @@
+package com.davidcryer.trumpquotes.android.view.ui.helpers;
+
+import android.view.View;
+
+public class ViewHelper {
+
+    public static boolean coordinatesAreInView(final float x, final float y, final View view) {
+        return true;//TODO
+    }
+
+    public static boolean coordinatesAreInView(final int x, final int y, final View view) {
+        return true;//TODO
+    }
+}
