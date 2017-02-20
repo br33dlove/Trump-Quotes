91166b44a1b816a320cc1aac9339876931aa3772
David Cryer <davidcryer@Davids-MBP.home>
2017-02-20 19:03:22 +0000

    amend fade animations

--- app/src/main/java/com/davidcryer/trumpquotes/android/view/ui/helpers/AlphaAnimationHelper.java
+++ app/src/main/java/com/davidcryer/trumpquotes/android/view/ui/helpers/AlphaAnimationHelper.java
@@ -5,29 +5,33 @@ import android.view.View;
 public class AlphaAnimationHelper {
 
     public static void fadeOut(final View view, final long maxDuration) {
-        view.animate()
-                .alpha(0)
-                .setDuration(alphaAnimationDuration(view.getAlpha(), 0, maxDuration))
-                .withEndAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        view.setVisibility(View.GONE);
-                    }
-                })
-                .start();
+        if (view.getVisibility() != View.GONE) {
+            view.animate()
+                    .alpha(0)
+                    .setDuration(alphaAnimationDuration(view.getAlpha(), 0, maxDuration))
+                    .withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            view.setVisibility(View.GONE);
+                        }
+                    })
+                    .start();
+        }
     }
 
     public static void fadeIn(final View view, final long maxDuration) {
-        view.animate()
-                .alpha(1)
-                .setDuration(alphaAnimationDuration(view.getAlpha(), 1, maxDuration))
-                .withStartAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        view.setVisibility(View.VISIBLE);
-                    }
-                })
-                .start();
+        if (view.getVisibility() != View.VISIBLE) {
+            view.animate()
+                    .alpha(1)
+                    .setDuration(alphaAnimationDuration(view.getAlpha(), 1, maxDuration))
+                    .withStartAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            view.setVisibility(View.VISIBLE);
+                        }
+                    })
+                    .start();
+        }
     }
 
     private static long alphaAnimationDuration(final float start, final float end, final long maxDuration) {
