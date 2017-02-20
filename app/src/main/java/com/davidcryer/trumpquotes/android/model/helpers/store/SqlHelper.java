98ff2e4e8d4b46302acef0091812bacf9e05873b
David Cryer <davidcryer@Davids-MBP.home>
2017-02-20 23:10:13 +0000

    changes to SQLiteHelper

--- app/src/main/java/com/davidcryer/trumpquotes/android/model/helpers/store/SqlHelper.java
+++ app/src/main/java/com/davidcryer/trumpquotes/android/model/helpers/store/SqlHelper.java
@@ -1,13 +1,15 @@
 package com.davidcryer.trumpquotes.android.model.helpers.store;
 
 public class SQLHelper {
-    public static final String SPACE = " ";
-    public static final String OPEN_BRACKET = "(";
-    public static final String CLOSE_BRACKET = ")";
-    public static final String COMMA = ", ";
-    public static final String CREATE_TABLE = "CREATE TABLE ";
-    public static final String TEXT_TYPE = " TEXT";
-    public static final String INTEGER_TYPE = " INTEGER";
-    public static final String PRIMARY_KEY_TYPE = " PRIMARY KEY";
-    public static final String ORDER_ASCENDING = " ASC";
+    public final static String SPACE = " ";
+    public final static String OPEN_BRACKET = "(";
+    public final static String CLOSE_BRACKET = ")";
+    public final static String COMMA = ", ";
+    public final static String CREATE_TABLE = "CREATE TABLE ";
+    public final static String TEXT_TYPE = " TEXT";
+    public final static String INTEGER_TYPE = " INTEGER";
+    public final static String PRIMARY_KEY_TYPE = " PRIMARY KEY";
+    public final static String ORDER_ASCENDING = " ASC";
+    public final static int INT_VALUE_TRUE = 1;
+    public final static int INT_VALUE_FALSE = 0;
 }
