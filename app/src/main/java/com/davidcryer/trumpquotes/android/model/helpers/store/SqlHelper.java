6eb88c4d76f9e661d77f857ee24e6f0c66d6f937
David Cryer <davidcryer@Davids-MBP.home>
2017-02-21 23:09:42 +0000

    made SqlHelper an interface

--- app/src/main/java/com/davidcryer/trumpquotes/android/model/helpers/store/SqlHelper.java
+++ app/src/main/java/com/davidcryer/trumpquotes/android/model/helpers/store/SqlHelper.java
@@ -1,15 +1,15 @@
 package com.davidcryer.trumpquotes.android.model.helpers.store;
 
-public class SQLHelper {
-    public final static String SPACE = " ";
-    public final static String OPEN_BRACKET = "(";
-    public final static String CLOSE_BRACKET = ")";
-    public final static String COMMA = ", ";
-    public final static String CREATE_TABLE = "CREATE TABLE ";
-    public final static String TEXT_TYPE = " TEXT";
-    public final static String INTEGER_TYPE = " INTEGER";
-    public final static String PRIMARY_KEY_TYPE = " PRIMARY KEY";
-    public final static String ORDER_ASCENDING = " ASC";
-    public final static int INT_VALUE_TRUE = 1;
-    public final static int INT_VALUE_FALSE = 0;
+public interface SQLHelper {
+    String SPACE = " ";
+    String OPEN_BRACKET = "(";
+    String CLOSE_BRACKET = ")";
+    String COMMA = ", ";
+    String CREATE_TABLE = "CREATE TABLE ";
+    String TEXT_TYPE = " TEXT";
+    String INTEGER_TYPE = " INTEGER";
+    String PRIMARY_KEY_TYPE = " PRIMARY KEY";
+    String ORDER_ASCENDING = " ASC";
+    int INT_VALUE_TRUE = 1;
+    int INT_VALUE_FALSE = 0;
 }
